5ae9ae7d9f675b1900e91f5ab70c08893f8cc9d6
David Pisoni <david_pisoni@intuit.com>
2025-02-26 10:37:50 -0800

    Fixed ConversationUpdate to properly support tool calls

--- Sources/Models/ConversationUpdate.swift
+++ Sources/Models/ConversationUpdate.swift
@@ -5,12 +5,28 @@ public struct Message: Codable {
         case user = "user"
         case assistant = "assistant"
         case system = "system"
+        case tool = "tool"
+    }
+    
+    public struct ToolCall: Codable {
+        public let id: String
+        public let type: String // possibly should be an enum
+        public let function: FunctionCall
+    }
+    
+    public struct FunctionCall: Codable {
+        public let name: String
+        public let arguments: String
     }
     
     public let role: Role
-    public let content: String
+    public let content: String?
+    public let tool_calls: [ToolCall]?
+    public let tool_call_id: String?
 }
 
 public struct ConversationUpdate: Codable {
     public let conversation: [Message]
+    // public let messages: [Message]?
+    // public let messagesOpenAIFormatted: [Message]?
 }
